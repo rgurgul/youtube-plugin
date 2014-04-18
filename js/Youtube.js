cc8b7ab176ad991dec5c9f80b92ef6c1b281a8f1
Robert <czniach@gmail.com>
2014-04-19 01:07:17 +0200

    refactor

--- js/Youtube.js
+++ js/Youtube.js
@@ -39,15 +39,15 @@ Youtube.prototype.getFeeds = function(state)
 	this.state = state;
 	var rootLink;
 	var query;
-	
+
 	if(state == 'videos')
 	{
 		rootLink = "http://gdata.youtube.com/feeds/api/videos?author=" + this.user + "&v=1";
-	} 
+	}
 	else if(state == 'featured')
 	{
 		rootLink = "http://gdata.youtube.com/feeds/api/videos?author=" + this.user + "&v=1";
-	} 
+	}
 	else
 	{
 		rootLink = "http://gdata.youtube.com/feeds/users/" + user +"/uploads?v=1";
@@ -57,14 +57,14 @@ Youtube.prototype.getFeeds = function(state)
 	var maxResults = "&max-results=" + this.maxResults;
 	var orderby = "&orderby=published";
 	//var feature = "&feature=plcp";
-	
+
 	if(this.userQuery)
 	{
 		query = rootLink + alt + maxResults + '&q=' + this.userQuery + orderby;
 	}else{
 		query = rootLink + alt + maxResults + orderby;
 	}
-	
+
 	$.ajax({
 			url: query,
 			dataType: 'jsonp',
@@ -81,7 +81,7 @@ Youtube.prototype.youtube_callback = function(data)
 	//this.fire("reloadVideo");
 };
 
-Youtube.prototype.loadVideo = function(playerUrl, autoplay, title, author, desc, viewCount, url) 
+Youtube.prototype.loadVideo = function(playerUrl, autoplay, title, author, desc, viewCount, url)
 {
 	swfobject.embedSWF(playerUrl + '&rel=1&border=0&showinfo=0&fs=1&autoplay='
 			+ (autoplay ? 1 : 0), this.playerContainer, '560', '315', '9.0.0', false,
@@ -99,14 +99,12 @@ Youtube.prototype.loadVideo = function(playerUrl, autoplay, title, author, desc,
 	this.fire("createNewLikeButton");
 };
 
-Youtube.prototype.showMyVideos = function(data) 
+Youtube.prototype.showMyVideos = function(data)
 {
-	//alert(idFirstVideo);
-	//alert(JSON.stringify(data));
 	this.idFirstVideo = 0;
 	var feed = data.feed;
 	var entries = feed.entry || [];
-	
+
 	if(entries.length == 0)
 	{
 		this.videosThumbs.innerHTML = "<p style='color: #9C0B08; font-weight: bold;' >" + cfg.getText('alertNoResult') + "</p>";
@@ -115,22 +113,20 @@ Youtube.prototype.showMyVideos = function(data)
 	}
 
 	var html = [ '<ul class="videos">' ];
-	for ( var i = 0; i < entries.length; i++) 
+	for ( var i = 0; i < entries.length; i++)
 	{
 		var entry = entries[i];
 		var tmp = entry.title.$t;
 		var title = tmp.replace("'", "");
-		
 		var tmp = entry.media$group.media$description.$t;
 		var tmp1 = tmp.replace("'", "");
 		var opis = tmp1.replace(/'/g, "&apos;").replace(/"/g, "&quot;") + ' ';
-		
+
 		if(title == this.firstDisplay)
 		{
 			this.idFirstVideo = i;
 			continue;
 		}
-		//alert(entry[ "yt$statistics" ].viewCount);
 		var thumbnailUrl = entry.media$group.media$thumbnail[0].url;
 		var playerUrl = entry.media$group.media$content[0].url;
 		var url = entry.media$group.media$player[0].url;
@@ -140,43 +136,42 @@ Youtube.prototype.showMyVideos = function(data)
 			 stat = entry[ "yt$statistics" ].viewCount;
 			}
 			catch(e){
-			 //catch and just suppress error
-			}		
+			}
 		var viewCount = stat;
-		
-		var func = "loadVideoMain('" 
-			+ playerUrl + "','" 
-			+ true + "','" 
-			+ title + "','" 
-			+ author + "','" 
-			+ opis + "','" 
-			+ viewCount + "','" 
+
+		var func = "loadVideoMain('"
+			+ playerUrl + "','"
+			+ true + "','"
+			+ title + "','"
+			+ author + "','"
+			+ opis + "','"
+			+ viewCount + "','"
 			+ url + "')";
-		
+
 		html.push('<li class=thumbbox-back onclick="', func, '">', '<img src="',
 				thumbnailUrl, '" />', '<div class=\'txt-container\'><p class="titlec">', title,
-				'</p><span class="video-view-count">', cfg.getText('numberDisplay'), ': ', 
+				'</p><span class="video-view-count">', cfg.getText('numberDisplay'), ': ',
 				viewCount, '</span>',
 				'<p class="opis">', opis,
 				'</p></div></li>');
 	}
-	html.push('</ul><br style="clear: left;"/>');
-	this.videosThumbs.innerHTML = html.join('');
-	if (entries.length > 0) 
-	{
-		var stat = 'brak danych';
-		try{
-			 stat = entries[this.idFirstVideo][ "yt$statistics" ].viewCount;
-			}
-			catch(e){
-			 //catch and just suppress error
-			}
-		this.loadVideo(entries[this.idFirstVideo].media$group.media$content[0].url, true,
-				entries[this.idFirstVideo].title.$t, entries[this.idFirstVideo].author[0].name.$t,
-				entries[this.idFirstVideo].media$group.media$description.$t + ' ',
-				stat,
-				entries[this.idFirstVideo].media$group.media$player[0].url);
-		
-		this.fire("reloadVideo");
-	}
+    html.push('</ul><br style="clear: left;"/>');
+    this.videosThumbs.innerHTML = html.join('');
+    if (entries.length > 0) {
+        var stat = 'brak danych';
+        try {
+            stat = entries[this.idFirstVideo][ "yt$statistics" ].viewCount;
+        } catch (e) {
+        }
+        this.loadVideo(entries[this.idFirstVideo].media$group.media$content[0].url,
+            true,
+            entries[this.idFirstVideo].title.$t,
+            entries[this.idFirstVideo].author[0].name.$t,
+                entries[this.idFirstVideo].media$group.media$description.$t
+                + ' ',
+            stat,
+            entries[this.idFirstVideo].media$group.media$player[0].url);
+
+        this.fire("reloadVideo");
+    }
 };
